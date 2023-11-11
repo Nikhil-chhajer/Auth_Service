d3ba4d8da3201aa7d7b4813fe22e1b4875eed2ea
Nikhil-chhajer <nikhil.chhajer80@gmail.com>
2023-11-11 20:10:47 +0530

    first commit project skeleton setup

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const express=require('express');
+const app=express();
+const {PORT}=require('./config/serverConfig');
+const prepareAndStartServer=async ()=>{
+       app.listen(PORT,()=>{
+        console.log("Server Started at ",PORT);
+       })
+}
+prepareAndStartServer();
