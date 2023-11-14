24cf6bc9e0aeb4a8bb7440a33cc70a1e706e9b26
Nikhil-chhajer <nikhil.chhajer80@gmail.com>
2023-11-14 13:31:23 +0530

    sigup logic with password encryption

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 const express=require('express');
 const app=express();
+const apiRoutes=require('./routes/index');
 const {PORT}=require('./config/serverConfig');
+const bodyParser = require('body-parser');
 const prepareAndStartServer=async ()=>{
+       app.use(bodyParser.json());
+       app.use(bodyParser.urlencoded({
+              extended:true
+       }));
        app.listen(PORT,()=>{
         console.log("Server Started at ",PORT);
        })
+       app.use('/api',apiRoutes);
 }
 prepareAndStartServer();
